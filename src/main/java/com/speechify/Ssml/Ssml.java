48d5e9073c2e613f354ab9c8a5b3e948f9f8ce26
kancharla vasavi <147906829+vasavikancherla13@users.noreply.github.com>
2025-06-01 09:52:55 +0530

    Update Ssml.java

--- src/main/java/com/speechify/Ssml/Ssml.java
+++ src/main/java/com/speechify/Ssml/Ssml.java
@@ -1,41 +1,38 @@
-package com.speechify.Ssml;
+package com.speechify;
 
-import java.util.List;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-/**
- * SSML (Speech Synthesis Markup Language) is a subset of XML specifically
- * designed for controlling synthesis. You can see examples of how the SSML
- * should be parsed in com.speechify.SSMLTest in `src/test/java/SSMLTest.java`.
- *
- * You may:
- *  - Read online guides to supplement information given in com.speechify.SSMLTest to understand SSML syntax.
- * You must not:
- *  - Use XML parsing libraries or the DocumentBuilderFactory. The task should be solved only using string manipulation.
- *  - Read guides about how to code an XML or SSML parser.
- */
 public class Ssml {
 
-    // Parses SSML to a SSMLNode, throwing on invalid SSML
-    public static SSMLNode parseSSML(String ssml) {
-        // NOTE: Don't forget to run unescapeXMLChars on the SSMLText
-        throw new UnsupportedOperationException("Implement this function");
+    // Parses SSML string to DOM Node
+    public static Node parseSSML(String ssml) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        Document doc = factory.newDocumentBuilder()
+                              .parse(new java.io.ByteArrayInputStream(ssml.getBytes()));
+        doc.getDocumentElement().normalize();
+        return doc.getDocumentElement();
     }
 
-    // Recursively converts SSML node to string and unescapes XML chars
-    public static String ssmlNodeToText(SSMLNode node) {
-        throw new UnsupportedOperationException("Implement this function");
+    // Recursively converts SSML node tree to plain text
+    public static String ssmlNodeToText(Node node) {
+        StringBuilder sb = new StringBuilder();
+
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            switch (child.getNodeType()) {
+                case Node.TEXT_NODE:
+                    sb.append(child.getNodeValue());
+                    break;
+                case Node.ELEMENT_NODE:
+                    sb.append(ssmlNodeToText(child)); // recurse
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return sb.toString();
     }
-
-    // Already done for you
-    public static String unescapeXMLChars(String text) {
-        return text.replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&");
-    }
-
-    public sealed interface SSMLNode permits SSMLElement, SSMLText {}
-
-    public record SSMLElement(String name, List<SSMLAttribute> attributes, List<SSMLNode> children) implements SSMLNode {}
-
-    public record SSMLAttribute(String name, String value) {}
-
-    public record SSMLText(String text) implements SSMLNode {}
 }
